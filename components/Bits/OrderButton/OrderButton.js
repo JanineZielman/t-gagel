b42838f05185051c5af3c90c4be1577dd3fbc918
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-02-25 16:51:48 +0100

    update custom button

--- components/Bits/OrderButton/OrderButton.js
+++ components/Bits/OrderButton/OrderButton.js
@@ -2,11 +2,11 @@ import React from "react"
 import styles from "./OrderButton.module.scss"
 import Link from "next/link"
 
-const OrderButton = ({ children, link }) => {
+const OrderButton = ({ link, text }) => {
   return (
     <div className={styles.orderButton}>
       <Link href={link} className={styles.text}>
-        {children}
+        {text}
       </Link>
     </div>
   )
