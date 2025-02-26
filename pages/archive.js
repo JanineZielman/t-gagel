b7f53f9fe1e5dc69619d087793b6857cebb9e024
Ibo <iboibelings@gmail.com>
2025-02-26 14:24:52 +0100

    add HomeButton to archive page; comment out ArchiveMenu and adjust global styles

--- pages/archive.js
+++ pages/archive.js
@@ -12,6 +12,8 @@ import {
   SEO,
 } from "../components"
 import { getNextStaticProps } from "@faustwp/core"
+import { HomeButton } from "../components/Bits/HomeButton"
+
 
 export default function Page(props) {
   const { data } = useQuery(Page.query, {
@@ -37,20 +39,26 @@ export default function Page(props) {
       />
       <Main>
         <Container>
-          <ArchiveMenu />
+          <HomeButton />
+          {/* <ArchiveMenu /> */}
           <h1>Archief</h1>
           <div className="post-grid">
             {posts.map((item, i) => {
-              return(
-                <a className={`post-item ${item.node.categories.edges[0].node.name.toLowerCase()}`} href={`/posts/${item.node.slug}`}>
+              return (
+                <a
+                  className={`post-item ${item.node.categories.edges[0].node.name.toLowerCase()}`}
+                  href={`/posts/${item.node.slug}`}
+                >
                   <div>
-                    <span className='category'>{item.node.categories.edges[0].node.name}</span>
+                    <span className="category">
+                      {item.node.categories.edges[0].node.name}
+                    </span>
                     <h2>{item.node.title}</h2>
                   </div>
                   <div>
                     <p>{item.node.author.node.name}</p>
-                    <div className='img-wrapper'>
-                      <img src={item.node.featuredImage?.node.sourceUrl}/>
+                    <div className="img-wrapper">
+                      <img src={item.node.featuredImage?.node.sourceUrl} />
                     </div>
                   </div>
                 </a>
@@ -74,7 +82,10 @@ Page.query = gql`
     generalSettings {
       ...BlogInfoFragment
     }
-    headerMenuItems: menuItems(where: { location: $headerLocation } first: 50) {
+    headerMenuItems: menuItems(
+      where: { location: $headerLocation }
+      first: 50
+    ) {
       nodes {
         ...NavigationMenuItemFragment
       }
