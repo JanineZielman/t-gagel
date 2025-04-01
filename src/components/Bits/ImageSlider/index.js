5ef74814d3d1cfb0b2741495b77b5a7b306e9b0f
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2025-04-01 18:24:25 +0200

    slick slider

--- src/components/Bits/ImageSlider/index.js
+++ src/components/Bits/ImageSlider/index.js
@@ -1,94 +1,41 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "../../image";
 import styles from "./ImageSlider.module.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ImageSlider = ({ images }) => {
-  const totalSlides = images.length;
-  const visibleSlides = 3;
-  const slideMargin = 10;
-  const infiniteImages = [...images.slice(-visibleSlides), ...images, ...images.slice(0, visibleSlides)];
-  const [currentIndex, setCurrentIndex] = useState(visibleSlides);
-  const sliderRef = useRef(null);
-  const isTransitioning = useRef(false);
-  
-  useEffect(() => {
-    if (sliderRef.current && !isTransitioning.current) {
-      sliderRef.current.style.transition = "transform 0.3s ease-in-out";
-    }
-    
-    if (currentIndex === infiniteImages.length - visibleSlides) {
-      isTransitioning.current = true;
-      setTimeout(() => {
-        if (sliderRef.current) {
-          sliderRef.current.style.transition = "none";
-        }
-        setCurrentIndex(visibleSlides);
-        isTransitioning.current = false;
-      }, 0);
-    } else if (currentIndex === 0) {
-      isTransitioning.current = true;
-      setTimeout(() => {
-        if (sliderRef.current) {
-          sliderRef.current.style.transition = "none";
-        }
-        setCurrentIndex(infiniteImages.length - (2 * visibleSlides));
-        isTransitioning.current = false;
-      }, 0);
-    }
-  }, [currentIndex, infiniteImages.length, visibleSlides]);
-
-  const nextSlide = () => {
-    if (!isTransitioning.current) {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (!isTransitioning.current) {
-      setCurrentIndex((prevIndex) => prevIndex - 1);
-    }
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    centerPadding: '60px',
   };
 
   return (
     <div className={styles.sliderContainer}>
-      <div className={styles.sliderWrapper}>
-        <div
-          ref={sliderRef}
-          className={styles.slider}
-          style={{
-            transform: `translateX(calc(-${currentIndex * (100 / visibleSlides)}% + ${slideMargin}px))`,
-            display: "flex",
-          }}
-        >
-          {infiniteImages.map((image, index) => (
+
+      <Slider {...settings}>
+          {images.map((image, index) => (
             <div
               key={index}
               className={styles.slide}
-              style={{
-                flex: `0 0 calc(${100 / visibleSlides}% - ${slideMargin * 2}px)`,
-                margin: `0 ${slideMargin}px`,
-              }}
             >
               <Image
                 customImageId={image.image}
                 altText={image.alt || ""}
-                width={image.width || 800}
-                height={image.height || 600}
-                layout="responsive"
+                layout="fill"
                 objectFit="cover"
               />
             </div>
           ))}
-        </div>
-      </div>
-
-      <button className={`${styles.navigationButton} ${styles.prev}`} onClick={prevSlide}>
-        ‹
-      </button>
-
-      <button className={`${styles.navigationButton} ${styles.next}`} onClick={nextSlide}>
-        ›
-      </button>
+          
+      </Slider>
     </div>
   );
 };
--- src/components/image/index.js
+++ src/components/image/index.js
@@ -3,16 +3,7 @@ import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import cx from "classnames"
 
-/**
- * Image Component.
- * We don't need to add srcSet, as Next js will generate that.
- * @see https://nextjs.org/docs/api-reference/next/image#other-props
- * @see https://nextjs.org/docs/basic-features/image-optimization#device-sizes
- *
- * @param {Object} props Component props.
- *
- * @return {jsx}
- */
+
 const Image = (props) => {
   const {
     altText,
@@ -57,25 +48,10 @@ const Image = (props) => {
     return null
   }
 
-  /**
-   * If we use layout = fill then, width and height of the image cannot be used.
-   * and the image fills on the entire width and the height of its parent container.
-   * That's we need to wrap our image in a container and give it a height and width.
-   * Notice that in this case, the given height and width is being used for container and not img.
-   */
   if ("fill" === layout) {
-    const attributes = {
-      alt: altText || title,
-      src: finalSourceUrl,
-      layout: "fill",
-      className: cx("object-cover", className),
-      loading: "lazy", // Enable lazy loading
-      ...rest,
-    }
-
     return (
       <div className={cx("relative", containerClassNames)}>
-        <Img {...attributes} />
+        <img  src={finalSourceUrl} />
       </div>
     )
   } else {
