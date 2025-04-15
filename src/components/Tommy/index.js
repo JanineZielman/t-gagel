dc3f4e45bd77e894cba81c6809ab10bff548fee6
Ibo <info@iboibelings.nl>
2025-04-15 14:54:43 +0200

    mini zoek en boek

--- src/components/Tommy/index.js
+++ src/components/Tommy/index.js
@@ -2,10 +2,6 @@ import styles from "./campingBoeken.module.scss"
 import { useEffect } from "react";
 
 const Tommy = ({ product }) => {
-
-
-
-
   useEffect(() => {
     console.log("Loading Tommy widget and matrix scripts");
 
@@ -32,24 +28,23 @@ const Tommy = ({ product }) => {
     };
   }, []);
 
-
   return (
     <div className={styles.container}>
-      <tommy-widget
+      {/* <tommy-widget
         data-widget="default"
         data-api-token="GagelLochem:6501dc199c514235a989b6e8c637137cc3a99211b139a3ce8c2d6229863507b7"
         data-language="nl"
         data-period="day"
         data-booking-url="/overnachten/reserveren"
-      ></tommy-widget>
+      ></tommy-widget> */}
 
-      {/* <h2>Zoek en boek!</h2>
+      <h2>Zoek en boek!</h2>
       <tommy-widget
         data-widget="mini"
         data-api-token="GagelLochem:6501dc199c514235a989b6e8c637137cc3a99211b139a3ce8c2d6229863507b7"
         data-language="nl"
         data-base-redirect-url="/overnachten/reserveren"
-      ></tommy-widget> */}
+      ></tommy-widget> 
     </div>
   )
 }
