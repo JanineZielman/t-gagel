6c5e5202d34bc71552e5fbb39313808bf582e948
Ibo <info@iboibelings.nl>
2025-04-15 14:37:33 +0200

    tommytest

--- src/components/Tommy/index.js
+++ src/components/Tommy/index.js
@@ -1,6 +1,38 @@
 import styles from "./campingBoeken.module.scss"
+import { useEffect } from "react";
 
 const Tommy = ({ product }) => {
+
+
+
+
+  useEffect(() => {
+    console.log("Loading Tommy widget and matrix scripts");
+
+    // Load Tommy widget script
+    const widgetScript = document.createElement("script");
+    widgetScript.src = "https://api.tommybookingsupport.com/widgets/zoekenboek/js/init.js";
+    widgetScript.async = true;
+
+    // Load Tommy matrix script
+    const matrixScript = document.createElement("script");
+    matrixScript.src = "https://www.tommybookingsupport.com/widget/js/tommy.matrix.js";
+    matrixScript.async = true;
+
+    document.body.appendChild(widgetScript);
+    // document.body.appendChild(matrixScript);
+
+    return () => {
+      if (document.body.contains(widgetScript)) {
+        document.body.removeChild(widgetScript);
+      }
+      if (document.body.contains(matrixScript)) {
+        document.body.removeChild(matrixScript);
+      }
+    };
+  }, []);
+
+
   return (
     <div className={styles.container}>
       <tommy-widget
