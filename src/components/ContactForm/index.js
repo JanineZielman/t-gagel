94654cab8e524e50752abbf120998c85f664c8b4
Ibo <iboibelings@gmail.com>
2025-05-03 12:59:03 +0200

    added brevo to contactform

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -200,16 +200,16 @@ const ContactForm = ({ backgroundColor, textColor }) => {
         )} */}
 
         <div className="sib-form">
-        {/* <div className={`sib-form ${styles.formContainer}`}> */}
+          {/* <div className={`sib-form ${styles.formContainer}`}> */}
           <div id="sib-form-container" className="sib-form-container">
             <div id="error-message" className="sib-form-message-panel">
               <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
-                <svg
+                {/* <svg
                   viewBox="0 0 512 512"
                   className="sib-icon sib-notification__icon"
                 >
                   <path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
-                </svg>
+                </svg> */}
                 <span className="sib-form-message-panel__inner-text">
                   Your subscription could not be saved. Please try again.
                 </span>
@@ -218,12 +218,12 @@ const ContactForm = ({ backgroundColor, textColor }) => {
             <div></div>
             <div id="success-message" className="sib-form-message-panel">
               <div className="sib-form-message-panel__text sib-form-message-panel__text--center">
-                <svg
+                {/* <svg
                   viewBox="0 0 512 512"
                   className="sib-icon sib-notification__icon"
                 >
                   <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
-                </svg>
+                </svg> */}
                 <span className="sib-form-message-panel__inner-text">
                   Je bericht is verzonden!
                 </span>
@@ -232,7 +232,8 @@ const ContactForm = ({ backgroundColor, textColor }) => {
             <div></div>
             <div
               id="sib-container"
-              className={`sib-container--large sib-container--vertical ${styles.formContainer}`}
+              // className={`sib-container--large sib-container--vertical ${styles.formContainer}`}
+              className="sib-container--large sib-container--vertical"
             >
               <form
                 id="sib-form"
@@ -245,7 +246,7 @@ const ContactForm = ({ backgroundColor, textColor }) => {
                   <div className="sib-input sib-form-block">
                     <div className="form__entry entry_block">
                       <div className="form__label-row ">
-                        <div className="">
+                        <div className="entry__field">
                           <input
                             className="input "
                             maxlength="200"
@@ -268,7 +269,7 @@ const ContactForm = ({ backgroundColor, textColor }) => {
                   <div className="sib-input sib-form-block">
                     <div className="form__entry entry_block">
                       <div className="form__label-row ">
-                        <div className="">
+                        <div className="entry__field">
                           <input
                             className="input "
                             type="text"
@@ -290,7 +291,7 @@ const ContactForm = ({ backgroundColor, textColor }) => {
                   <div className="sib-input sib-form-block">
                     <div className="form__entry entry_block">
                       <div className="form__label-row ">
-                        <div className="">
+                        <div className="entry__field">
                           <input
                             className="input "
                             maxlength="200"
@@ -313,23 +314,9 @@ const ContactForm = ({ backgroundColor, textColor }) => {
                   <div className="sib-input sib-form-block">
                     <div className="form__entry entry_block">
                       <div className="form__label-row ">
-                        <div className="">
-                          {/* <input
-                            className="input "
-                            maxlength="200"
-                            type="text"
-                            id="TOPIC"
-                            name="TOPIC"
-                            autocomplete="off"
-                            placeholder="TOPIC"
-                            data-required="true"
-                            required
-                          /> */}
+                        <div className="entry__field">
                           <select
-
-                            // value={formData.Topic}
-                            // onChange={handleChange}
-                            className={styles.select}
+                            className="input "
                             id="TOPIC"
                             name="TOPIC"
                             required
@@ -367,10 +354,10 @@ const ContactForm = ({ backgroundColor, textColor }) => {
                   <div className="sib-input sib-form-block">
                     <div className="form__entry entry_block">
                       <div className="form__label-row ">
-                        <div className="">
-                          <input
-                            className="input "
-                            maxlength="200"
+                        <div className="entry__field">
+                          <textarea
+                            className="input textarea"
+                            // maxlength="200"
                             type="text"
                             id="MESSAGE"
                             name="MESSAGE"
